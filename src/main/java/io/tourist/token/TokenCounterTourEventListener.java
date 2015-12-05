ee4d512b5d753cda00b33cdd2d8a90fc556ad788
Diego Pettisani <DPETTISANI@DPETTISANI-PC.station>
2015-12-05 01:09:36 +0100

    Initial commit

---
+++ src/main/java/io/tourist/token/TokenCounterTourEventListener.java
@@ -0,0 +1,33 @@
+package io.tourist.token;
+
+import io.tourist.core.api.Tour;
+import io.tourist.core.event.TourEventListenerAdapter;
+
+public class TokenCounterTourEventListener extends TourEventListenerAdapter {
+
+	@Override
+	public void onTouristTravelStarted(Tour tour) {
+
+	}
+
+	@Override
+	public void onTourStarted(Tour tour) {
+
+	}
+
+	@Override
+	public void onTourEnded(Tour tour) {
+
+	}
+
+	@Override
+	public void onTourFailed(Tour tour) {
+
+	}
+
+	@Override
+	public void onTouristTravelEnded(Tour tour) {
+
+	}
+
+}
